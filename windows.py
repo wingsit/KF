f45d8e83c2549bfece440cbc8958891da040604f
wingsit-laptop <wing1127aishi@gmail.com>
2009-07-19 17:44:55 -0400

    Added the rolling window function

---
+++ windows.py
@@ -0,0 +1,40 @@
+import scipy, itertools
+
+
+def windows(iterable, window, overlap):
+    if not isinstance(iterable, scipy.matrix):
+        iterable = list(iterable)
+    i = 0
+    diff = window - overlap
+    while i <=len(iterable)-window:
+        yield iterable[i:i+window]
+        i += diff
+def fullRollingWindows(iterable, window, overlap):
+    if not isinstance(iterable, scipy.matrix):
+        iterable = list(iterable)
+    i = -overlap
+    diff = window - overlap
+    while i <len(iterable):
+        if i < 0:
+            yield iterable[:window]
+        elif i + window > len(iterable):
+            yield iterable[-window:]
+        else: 
+            yield iterable[i:i+window]
+        i += diff
+
+def main():
+    l = xrange(0,30)
+    m = scipy.matrix(xrange(60)).reshape((2,30)).T
+#     for i in matrixwindows(m,10,9):
+#         print i
+#     for i in rollingwindows(l,10,9):
+#         print i
+#    for i in matrixwindows(m,7,4):
+#        print i
+    for i in fullRollingWindows(m,10,9):
+        print i
+
+if __name__ == "__main__":
+
+    main()
