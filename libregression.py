079053bda091a14b4ee92247bfc8b9aae7fbc9b5
wingsit-work <wing1127aishi@gmail.com>
2009-07-19 19:19:26 -0400

    Added the regression procedures

---
+++ libregression.py
@@ -0,0 +1,16 @@
+import scipy
+"""This file contains a list of algoritm that is used for estimation
+    some optmisation should be done in future.
+    Naming convention:
+        function with no ec/ic prefix are estimator for unconstrained problem
+        ec* prefix: this is for equality constraints.
+        ic* prefixL this is for inequality constraints."""
+
+
+def regression(X, y, W):
+    return (X.T * W * X).I*(X.T * W * y)
+
+def ecregression(X,y,W,D,d):
+    covinv = (X.T * W * X).I
+    lamb = (D * covinv * D.T).I * (D * covinv * X.T * W * y - d)
+    return  covinv * (X.T * W * y - D.T * lamb)
