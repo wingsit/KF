81c558d82f55be187ca27e4076dc92c5237e5e63
wingsit-laptop <wing1127aishi@gmail.com>
2009-10-13 23:34:37 -0400

    experimenting all kalman filter implementation and it works in libregression.py

--- kalman_filter/test_kalmanFilter.py
+++ kalman_filter/test_kalmanFilter.py
@@ -1,4 +1,4 @@
-import csv, scipy
+import csv, scipy, sys
 from libregression import *
 data = scipy.matrix(map(lambda x: map(float, x), csv.reader(open("test_dodge_cox.csv", "rb"))))
 print data
@@ -13,11 +13,16 @@ Phi = scipy.identity(n)
 S = scipy.identity(n)
 sigma = scipy.matrix([1.0])
 Sigma = scipy.identity(n)
+D = scipy.ones((1,n))
+d = scipy.matrix(1.)
+G = scipy.identity(n)
+a = scipy.zeros((n,1))
+c = scipy.ones((n,1))
 #        import code; code.interact(local=locals())
 (b, V) = kalman_predict(b,V,Phi, S)
 for i, (xs, ys) in enumerate(zip(X,y)):
     beta[i,:] = scipy.array(b).T
-    (b,V, e,K) = kalman_upd(b,V, ys ,xs, sigma, Sigma)
+    (b,V, e,K) = kalman_upd(b,V, ys ,xs, sigma, Sigma, 2, D, d,G,a,c)
 #    print "b:\n", b
 #    print "V:\n", V
 #    print "e:\n", e
