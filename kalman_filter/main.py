41660d8b34d18a83aa51aaa6044ca49b24e1d831
wingsit-laptop <wing1127aishi@gmail.com>
2009-10-23 20:21:20 -0400

    Commit after Flexible least Square

--- kalman_filter/main.py
+++ kalman_filter/main.py
@@ -15,41 +15,41 @@ def main():
     respond = stock[:,0]
     regressors = stock[:,1:]
     t, n= regressors.size()
-#    obj = Regression(respond, regressors, intercept).train().getEstimate().plot()
+    obj = Regression(respond, regressors, intercept).train().getEstimate().plot()
 
     D = scipy.ones((1,n))
     d = scipy.matrix(1.0)
-#    obj = ECRegression(respond, regressors, intercept, D,d).train().getEstimate().plot()
+    obj = ECRegression(respond, regressors, intercept, D,d).train().getEstimate().plot()
 
     a = scipy.zeros((n,1))
     b = scipy.ones((n,1))
     G = scipy.identity(n)
-#    obj = ICRegression(respond, regressors, intercept, D,d,G,a,b).train().getEstimate().plot()
+    obj = ICRegression(respond, regressors, intercept, D,d,G,a,b).train().getEstimate().plot()
 
 
     windowsize = 24
-##    obj = RollingRegression(respond,
-##                            regressors,
-##                            intercept,
-##                            weight = scipy.identity(WINDOWSIZE),
-##                            window = WINDOWSIZE).train().getEstimate().plot()
+    obj = RollingRegression(respond,
+                            regressors,
+                            intercept,
+                            weight = scipy.identity(WINDOWSIZE),
+                            window = WINDOWSIZE).train().getEstimate().plot()
 
-##    obj = ECRollingRegression(respond, regressors, intercept, D,d,weight = scipy.identity(WINDOWSIZE),
-##                              window = WINDOWSIZE).train().getEstimate().plot()
+    obj = ECRollingRegression(respond, regressors, intercept, D,d,weight = scipy.identity(WINDOWSIZE),
+                              window = WINDOWSIZE).train().getEstimate().plot()
     
-##    obj = ICRollingRegression(respond, regressors, intercept,
-##                              D,d,G,a,b,
-##                              weight = scipy.identity(WINDOWSIZE),window = WINDOWSIZE).train().getEstimate().plot()
+    obj = ICRollingRegression(respond, regressors, intercept,
+                              D,d,G,a,b,
+                              weight = scipy.identity(WINDOWSIZE),window = WINDOWSIZE).train().getEstimate().plot()
 
 
     
     initBeta = scipy.matrix([0.528744, 0.471256]).T
     Sigma = scipy.matrix([[0.123873, -0.12387], [-0.12387,0.123873]])
-#    obj = KalmanFilter(respond, regressors, intercept, Sigma, 0.12, initBeta = initBeta).train().getEstimate().plot()
-#    obj = ECKalmanFilter(respond, regressors, intercept, Sigma, 0.12, eta = initBeta).train().getEstimate().plot()
+    obj = KalmanFilter(respond, regressors, intercept, Sigma, 0.12, initBeta = initBeta).train().getEstimate().plot()
+    obj = ECKalmanFilter(respond, regressors, intercept, Sigma, 0.12, eta = initBeta).train().getEstimate().plot()
 
 
-#    obj = ICKalmanFilter(respond, regressors, intercept, Sigma, 0.12, initBeta = initBeta).train().getEstimate().plot()
+    obj = ICKalmanFilter(respond, regressors, intercept, Sigma, 0.12, initBeta = initBeta).train().getEstimate().plot()
     obj = ICKalmanFilter(respond, regressors, intercept, Sigma, 0.12, initBeta = initBeta).train()
     print obj.R2()
 if __name__ == "__main__":
